12291bd4b4d11c9771fa8f363829b0ca227a12f5
root <samfrexz69@gmail.com>
2023-09-30 02:18:10 -0700

    upd file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Flask web application API.
+'''Contains a Flask web application API.
 '''
 import os
 from flask import Flask, jsonify
@@ -10,7 +10,7 @@ from api.v1.views import app_views
 
 
 app = Flask(__name__)
-'''Flask web application instance.'''
+'''The Flask web application instance.'''
 app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 app_port = int(os.getenv('HBNB_API_PORT', '5000'))
 app.url_map.strict_slashes = False
@@ -20,23 +20,24 @@ CORS(app, resources={'/*': {'origins': app_host}})
 
 @app.teardown_appcontext
 def teardown_flask(exception):
-    '''Flask app/request.'''
+    '''The Flask app/request context end event listener.'''
+    # print(exception)
     storage.close()
 
 
 @app.errorhandler(404)
 def error_404(error):
-    '''Handles the 404.'''
+    '''Handles the 404 HTTP error code.'''
     return jsonify(error='Not found'), 404
 
 
 @app.errorhandler(400)
 def error_400(error):
-    '''Handles the 400 HTTP errore.'''
-    message = 'Bad request'
+    '''Handles the 400 HTTP error code.'''
+    msg = 'Bad request'
     if isinstance(error, Exception) and hasattr(error, 'description'):
-        message = error.description
-    return jsonify(error=message), 400
+        msg = error.description
+    return jsonify(error=msg), 400
 
 
 if __name__ == '__main__':
